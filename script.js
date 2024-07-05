ea63616ce7ff23fa92f4464b02b7361f653d691a
Aayushchauhan05 <140552699+Aayushchauhan05@users.noreply.github.com>
2024-07-05 19:55:07 +0530

    quiz add

---
+++ script.js
@@ -0,0 +1,550 @@
+const data = [
+  {
+    id: 1,
+    question: "Rolex is a company that specializes in what type of product?",
+    answers: [
+      {
+        text: "Phone",
+        correct: false,
+      },
+      {
+        text: "Watches",
+        correct: true,
+      },
+      {
+        text: "Food",
+        correct: false,
+      },
+      {
+        text: "Cosmetic",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 2,
+    question: "When did the website `Facebook` launch?",
+    answers: [
+      {
+        text: "2004",
+        correct: true,
+      },
+      {
+        text: "2005",
+        correct: false,
+      },
+      {
+        text: "2006",
+        correct: false,
+      },
+      {
+        text: "2007",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 3,
+    question: "Who played the character of harry potter in movie?",
+    answers: [
+      {
+        text: "Johnny Deep",
+        correct: false,
+      },
+      {
+        text: "Leonardo Di Caprio",
+        correct: false,
+      },
+      {
+        text: "Denzel Washington",
+        correct: false,
+      },
+      {
+        text: "Daniel Red Cliff",
+        correct: true,
+      },
+    ],
+  },
+  {
+    id: 4,
+    question: "Who is the prime minister of india",
+    answers: [
+      {
+        text: "Johnny Deep",
+        correct: false,
+      },
+      {
+        text: "Leonardo Di Caprio",
+        correct: false,
+      },
+      {
+        text: "Narendra modi",
+        correct: true,
+      },
+      {
+        text: "Rahul Gandhi",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 5,
+    question: "who is the god of cricket?",
+    answers: [
+      {
+        text: "Virat Kholi",
+        correct: false,
+      },
+      {
+        text: "Dhoni",
+        correct: false,
+      },
+      {
+        text: "Raina",
+        correct: false,
+      },
+      {
+        text: "Sachin Tendulkar",
+        correct: true,
+      },
+    ],
+  },
+  {
+    id: 6,
+    question: "Who is the CM minister of UP",
+    answers: [
+      {
+        text: "Johnny Deep",
+        correct: false,
+      },
+      {
+        text: "Yogi aditya nath",
+        correct: true,
+      },
+      {
+        text: "Narendra modi",
+        correct: false,
+      },
+      {
+        text: "Rahul Gandhi",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 7,
+    question: "capital of india",
+    answers: [
+      {
+        text: "New delhi",
+        correct: true,
+      },
+      {
+        text: "shimla",
+        correct: false,
+      },
+      {
+        text: "Kashmir",
+        correct: false,
+      },
+      {
+        text: "Uttar pradesh",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 8,
+    question: "who played the role of captain jack sparrow in pirates of carribean",
+    answers: [
+      {
+        text: "Johnny Deep",
+        correct: true,
+      },
+      {
+        text: "Leonardo Di Caprio",
+        correct: false,
+      },
+      {
+        text: "Narendra modi",
+        correct: false,
+      },
+      {
+        text: "Rahul Gandhi",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 9,
+    question: "Best anime series of all time",
+    answers: [
+      {
+        text: "AOT",
+        correct: false,
+      },
+      {
+        text: "Naruto",
+        correct: true,
+      },
+      {
+        text: "Bleach",
+        correct: false,
+      },
+      {
+        text: "One-piece",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 10,
+    question: "Who is the ghost of the uchiha",
+    answers: [
+      {
+        text: "Johnny Deep",
+        correct: false,
+      },
+      {
+        text: "Itachi uchicha",
+        correct: false,
+      },
+      {
+        text: "Madara uchiha",
+        correct: true,
+      },
+      {
+        text: "sasuke uchiha",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 11,
+    question: "Who is the last uchiha",
+    answers: [
+      {
+        text: "Johnny Deep",
+        correct: false,
+      },
+      {
+        text: "Itachi uchicha",
+        correct: false,
+      },
+      {
+        text: "Madara uchiha",
+        correct: false,
+      },
+      {
+        text: "sasuke uchiha",
+        correct: true,
+      },
+    ],
+  },
+  {
+    id: 12,
+    question: "Who is the god of shinobi",
+    answers: [
+      {
+        text: "hashirama senju",
+        correct: true,
+      },
+      {
+        text: "Itachi uchicha",
+        correct: false,
+      },
+      {
+        text: "Madara uchiha",
+        correct: false,
+      },
+      {
+        text: "sasuke uchiha",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 13,
+    question: "who is the sensei of naruto after kakashi ?",
+    answers: [
+      {
+        text: "sunade",
+        correct: false,
+      },
+      {
+        text: "orochimaru",
+        correct: false,
+      },
+      {
+        text: "jiraya",
+        correct: true,
+      },
+      {
+        text: "sasuke uchiha",
+        correct: false,
+      },
+    ],
+  },
+  
+  {
+    id: 14,
+    question: "who also kmown as the the Tode sage?",
+    answers: [
+      {
+        text: "sunade",
+        correct: false,
+      },
+      {
+        text: "orochimaru",
+        correct: false,
+      },
+      {
+        text: "jiraya",
+        correct: true,
+      },
+      {
+        text: "sasuke uchiha",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 14,
+    question: "who also known as the the Tode sage?",
+    answers: [
+      {
+        text: "sunade",
+        correct: false,
+      },
+      {
+        text: "orochimaru",
+        correct: false,
+      },
+      {
+        text: "jiraya",
+        correct: true,
+      },
+      {
+        text: "sasuke uchiha",
+        correct: false,
+      },
+    ],
+  },
+   {
+    id: 15,
+    question: "sensei of sasuke",
+    answers: [
+      {
+        text: "sunade",
+        correct: false,
+      },
+      {
+        text: "orochimaru",
+        correct: true,
+      },
+      {
+        text: "jiraya",
+        correct: false,
+      },
+      {
+        text: "sasuke uchiha",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 13,
+    question: "who is the sensei of naruto after kakashi ?",
+    answers: [
+      {
+        text: "sunade",
+        correct: false,
+      },
+      {
+        text: "orochimaru",
+        correct: false,
+      },
+      {
+        text: "jiraya",
+        correct: true,
+      },
+      {
+        text: "sasuke uchiha",
+        correct: false,
+      },
+    ],
+  },
+
+  {
+    id: 14,
+    question: "who also kmown as the the Tode sage?",
+    answers: [
+      {
+        text: "sunade",
+        correct: false,
+      },
+      {
+        text: "orochimaru",
+        correct: false,
+      },
+      {
+        text: "jiraya",
+        correct: true,
+      },
+      {
+        text: "sasuke uchiha",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 15,
+    question: "who also known as the the Tode sage?",
+    answers: [
+      {
+        text: "sunade",
+        correct: false,
+      },
+      {
+        text: "orochimaru",
+        correct: false,
+      },
+      {
+        text: "jiraya",
+        correct: true,
+      },
+      {
+        text: "sasuke uchiha",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 16,
+    question: "sensei of sasuke",
+    answers: [
+      {
+        text: "sunade",
+        correct: false,
+      },
+      {
+        text: "orochimaru",
+        correct: true,
+      },
+      {
+        text: "jiraya",
+        correct: false,
+      },
+      {
+        text: "sasuke uchiha",
+        correct: false,
+      },
+    ],
+  },
+];
+
+let Qindex = 0;
+let score = 0;
+let timer;
+let timeLeft = 30;
+
+function startTimer() {
+  timeLeft = 30;
+  document.getElementById('time').textContent = timeLeft;
+  timer = setInterval(() => {
+      timeLeft--;
+      document.getElementById('time').textContent = timeLeft;
+      if (timeLeft <= 0) {
+          clearInterval(timer);
+          showScore();
+      }
+  }, 1000);
+}
+
+function showScore() {
+  document.querySelector('.quizcard').innerHTML = `<h2>Your score: ${score}</h2>`;
+  document.querySelector('.Next').disabled = true;
+  document.querySelector('.Prev').disabled = true;
+  document.querySelector('.Restart').style.display = 'inline-block';
+}
+
+function quiz() {
+  const supdiv = document.createElement("div");
+  supdiv.classList.add("supdiv");
+  const maindiv = document.createElement("div");
+  maindiv.classList.add("questiondiv");
+  const questionnum = document.createElement("p");
+  questionnum.textContent = `${data[Qindex].id}/${data.length}`;
+  maindiv.appendChild(questionnum);
+  const questionh1 = document.createElement("h1");
+  questionh1.textContent = data[Qindex].question;
+  maindiv.appendChild(questionh1);
+  document.querySelector(".quizcard").appendChild(maindiv);
+
+  data[Qindex].answers.forEach((Element) => {
+      const option = document.createElement("div");
+      option.classList.add("question");
+      const h1answer = document.createElement("h3");
+      h1answer.classList.add("h3answer");
+      h1answer.textContent = Element.text;
+      option.setAttribute("value", Element.correct);
+      option.appendChild(h1answer);
+      document.querySelector(".quizcard").appendChild(option);
+  });
+}
+
+quiz();
+startTimer();
+
+document.querySelector(".Next").addEventListener("click", () => {
+  Qindex++;
+  if (Qindex < data.length) {
+      document.querySelector(".quizcard").innerHTML = "";
+      quiz();
+  } else {
+      showScore();
+  }
+});
+
+document.querySelector(".quizcard").addEventListener("click", (event) => {
+  let selectedOption = event.target.closest(".question");
+  if (selectedOption) {
+      if (selectedOption.getAttribute("value") === "true") {
+          score++;
+          selectedOption.classList.add("correct");
+          setTimeout(() => {
+              if (Qindex < data.length - 1) {
+                  Qindex++;
+                  document.querySelector(".quizcard").innerHTML = "";
+                  quiz();
+              } else {
+                  showScore();
+              }
+          }, 500);
+      } else {
+          selectedOption.classList.add("incorrect");
+          const correctOption = document.querySelector(".question[value='true']");
+          correctOption.classList.add("correct");
+          setTimeout(showScore, 1000);
+      }
+  }
+});
+
+document.querySelector(".Prev").addEventListener("click", () => {
+  Qindex--;
+  if (Qindex >= 0) {
+      document.querySelector(".quizcard").innerHTML = "";
+      quiz();
+  }
+});
+
+document.querySelector(".Restart").addEventListener("click", () => {
+  Qindex = 0;
+  score = 0;
+  document.querySelector(".quizcard").innerHTML = "";
+  quiz();
+  document.querySelector('.Next').disabled = false;
+  document.querySelector('.Prev').disabled = false;
+  document.querySelector('.Restart').style.display = 'none';
+  startTimer();
+});
